79a45dfa84964e6a04c0e888f1918309e09aa3d7
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-02-15 21:56:57 +0100

    place and unplace tiles!

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,49 +1,114 @@
-import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Button, Input, Paper, styled } from '@mui/material';
+import { Button, styled } from '@mui/material';
 
 const defaultBoard = [
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-']
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '']
 ];
+
+const startingTiles = ['A', 'R', 'E', 'T', 'S', 'O', 'L'];
 
 export const Board: React.FC<{}> = () => {
   const [board, setBoard] = useState(defaultBoard);
+  const [tiles, setTiles] = useState(startingTiles);
+  const [unplayedBoard, setUnplayedBoard] = useState(defaultBoard);
+  const [selectedTile, setSelectedTile] = useState('');
   useEffect(() => {
     // herp
   }, []);
 
-  const onBoardEntry = (letter: string, row: number, column: number) => {
-    const copiedBoard = board.map((row) => row.map((cell) => cell));
-    copiedBoard[row][column] = letter;
-    setBoard(copiedBoard);
+  const selectTile = (tile: string) => {
+    console.log('select tile: ', tile);
+    setSelectedTile(tile);
+  };
+
+  const placeTile = (placedTile: string, row: number, column: number) => {
+    const copiedBoard = unplayedBoard.map((row) => row.map((cell) => cell));
+    const copiedTiles = [...tiles];
+
+    if (placedTile !== '') {
+      console.log('unplace tile', placedTile);
+      copiedTiles.push(placedTile);
+
+      copiedBoard[row][column] = '';
+    }
+    if (selectedTile !== '') {
+      console.log('place tile: ', selectedTile);
+
+      copiedBoard[row][column] = selectedTile;
+
+      const index = copiedTiles.indexOf(selectedTile);
+      if (index > -1) {
+        copiedTiles.splice(index, 1);
+      }
+      setSelectedTile('');
+    }
+
+    setTiles(copiedTiles);
+    setUnplayedBoard(copiedBoard);
   };
 
   return (
     <>
       <BoardGrid>
-        {board.map((row, indexRow) =>
+        {unplayedBoard.map((row, indexRow) =>
           row.map((cell, indexColumn) => (
             <BoardCell
+              onClick={() => placeTile(cell, indexRow, indexColumn)}
               key={indexRow * 100 + indexColumn}
-              defaultValue={cell}
-              onChange={(event) =>
-                onBoardEntry(event.target.value, indexRow, indexColumn)
-              }
-            />
+            >
+              {cell}
+            </BoardCell>
           ))
         )}
       </BoardGrid>
+      <TileHolder>
+        {tiles.map((tile, index) => (
+          <Tile
+            isSelected={tile === selectedTile}
+            key={index}
+            onClick={() => selectTile(tile)}
+          >
+            {tile}
+          </Tile>
+        ))}
+      </TileHolder>
       <Button variant="outlined">Spela ordet</Button>
     </>
   );
 };
+
+const TileHolder = styled('div')(() => ({
+  border: '2px solid teal',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(7, 1fr)',
+  gap: '0px 0px',
+  justifyItems: 'stretch'
+}));
+
+interface TileProps {
+  readonly isSelected: boolean;
+}
+
+const Tile = styled('div', {
+  shouldForwardProp: (prop) => prop !== 'isSelected'
+})<TileProps>(({ isSelected }) => ({
+  backgroundColor: isSelected ? 'lime' : 'white',
+  border: '1px solid red',
+  maxWidth: '100%',
+  width: '100%',
+  aspectRatio: '1',
+  fontSize: '1rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}));
 
 const BoardGrid = styled('div')(() => ({
   border: '2px solid green',
@@ -58,8 +123,14 @@ const BoardCellContainer = styled('div')(() => ({
   border: '2px solid red'
 }));
 
-const BoardCell = styled('input')((props) => ({
+const BoardCell = styled('div')((props) => ({
   border: '1px solid blue',
   maxWidth: '100%',
-  width: '100%'
+  width: '100%',
+  aspectRatio: '1',
+  fontSize: '1rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
 }));
