316fc616e95fa605bfd0050aa656a36fceada028
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-03-01 21:51:51 +0100

    typing Tile, introducing tile objects, preparing submit evaluation

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,42 +2,116 @@ import React, { useEffect, useState } from 'react';
 import { Button, styled } from '@mui/material';
 
 const defaultBoard = [
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '']
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ],
+  [
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' },
+    { letter: '', placed: 'no' }
+  ]
 ];
 
-const startingTiles = ['A', 'R', 'E', 'T', 'S', 'O', 'L'];
+const startingTiles: Tile[] = [
+  { letter: 'S', placed: 'hand' },
+  { letter: 'A', placed: 'hand' },
+  { letter: 'T', placed: 'hand' },
+  { letter: 'O', placed: 'hand' },
+  { letter: 'R', placed: 'hand' },
+  { letter: 'E', placed: 'hand' },
+  { letter: 'P', placed: 'hand' }
+];
+
+interface Tile {
+  letter: string;
+  placed: string;
+}
+
+const emptyTile: Tile = {
+  letter: '',
+  placed: 'no'
+};
 
 export const Board: React.FC<{}> = () => {
   const [board, setBoard] = useState(defaultBoard);
   const [tiles, setTiles] = useState(startingTiles);
   const [unplayedBoard, setUnplayedBoard] = useState(defaultBoard);
-  const [selectedTile, setSelectedTile] = useState('');
+  const [selectedTile, setSelectedTile] = useState<Tile>(emptyTile);
   useEffect(() => {
     // herp
   }, []);
 
-  const selectTile = (tile: string) => {
-    console.log('select tile: ', tile);
+  const selectTile = (tile: Tile) => {
+    console.log('select tile: ', tile.letter);
     setSelectedTile(tile);
   };
 
-  const placeTile = (placedTile: string, row: number, column: number) => {
+  const placeTile = (placedTile: Tile, row: number, column: number) => {
     const copiedBoard = unplayedBoard.map((row) => row.map((cell) => cell));
     const copiedTiles = [...tiles];
 
-    if (placedTile !== '') {
+    if (placedTile.letter !== emptyTile.letter) {
       console.log('unplace tile', placedTile);
       copiedTiles.push(placedTile);
 
-      copiedBoard[row][column] = '';
+      copiedBoard[row][column] = emptyTile;
     }
-    if (selectedTile !== '') {
+    if (selectedTile.letter !== emptyTile.letter) {
       console.log('place tile: ', selectedTile);
 
       copiedBoard[row][column] = selectedTile;
@@ -46,11 +120,22 @@ export const Board: React.FC<{}> = () => {
       if (index > -1) {
         copiedTiles.splice(index, 1);
       }
-      setSelectedTile('');
+      setSelectedTile(emptyTile);
     }
 
     setTiles(copiedTiles);
     setUnplayedBoard(copiedBoard);
+  };
+
+  const submitWord = () => {
+    console.log('submit word!');
+    unplayedBoard.map((row, indexRow) =>
+      row.map((cell, indexColumn) => {
+        if (cell.letter !== emptyTile.letter) {
+          return false;
+        }
+      })
+    );
   };
 
   return (
@@ -62,7 +147,7 @@ export const Board: React.FC<{}> = () => {
               onClick={() => placeTile(cell, indexRow, indexColumn)}
               key={indexRow * 100 + indexColumn}
             >
-              {cell}
+              {cell.letter}
             </BoardCell>
           ))
         )}
@@ -74,11 +159,13 @@ export const Board: React.FC<{}> = () => {
             key={index}
             onClick={() => selectTile(tile)}
           >
-            {tile}
+            {tile.letter}
           </Tile>
         ))}
       </TileHolder>
-      <Button variant="outlined">Spela ordet</Button>
+      <Button variant="outlined" onClick={() => submitWord()}>
+        Spela ordet
+      </Button>
     </>
   );
 };
@@ -103,7 +190,7 @@ const Tile = styled('div', {
   maxWidth: '100%',
   width: '100%',
   aspectRatio: '1',
-  fontSize: '1rem',
+  fontSize: '3rem',
 
   display: 'flex',
   alignItems: 'center',
@@ -128,7 +215,7 @@ const BoardCell = styled('div')((props) => ({
   maxWidth: '100%',
   width: '100%',
   aspectRatio: '1',
-  fontSize: '1rem',
+  fontSize: '3rem',
 
   display: 'flex',
   alignItems: 'center',
