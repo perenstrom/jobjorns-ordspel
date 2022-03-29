9dcf68c8ee617cf2bdf151ce1a5c311e69c78c0c
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-03-29 21:24:01 +0200

    Ny splash-sida, meny, och lite temaändringar

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,56 +1,98 @@
 import { useRouter } from 'next/router';
-import React from 'react';
-import Link from 'next/link';
-import { Paper, styled } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Toolbar,
+  Typography
+} from '@mui/material';
 
-interface MenuItemLiProps {
-  isActive: boolean;
-}
+import MenuIcon from '@mui/icons-material/Menu';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-const MenuItemLi = styled('li', {
-  shouldForwardProp: (prop) => prop !== 'isActive'
-})<MenuItemLiProps>((props) => ({
-  display: 'inline-block',
-  padding: props.theme.spacing(2),
-  backgroundColor: props.isActive
-    ? props.theme.palette.primary.light
-    : 'inherit'
-}));
-
-interface MenuItemProps {
-  target: string;
-}
-
-const StyledA = styled('a')((props) => ({
-  color: props.theme.palette.primary.contrastText,
-  textDecoration: 'none'
-}));
-
-const MenuItem: React.FC<MenuItemProps> = (props) => {
-  const router = useRouter();
-  return (
-    <MenuItemLi isActive={router.pathname === props.target}>
-      <Link href={props.target} passHref>
-        <StyledA>{props.children}</StyledA>
-      </Link>
-    </MenuItemLi>
-  );
-};
-
-const MenuBar = styled('ul')((props) => ({
-  color: props.theme.palette.primary.contrastText,
-  backgroundColor: props.theme.palette.primary.dark,
-  display: 'flexbox',
-  flexDirection: 'column'
-}));
+import { useUser } from '@auth0/nextjs-auth0';
 
 export const Menu: React.FC<{}> = () => {
+  const { user, error, isLoading } = useUser();
+
+  const [drawer, setDrawer] = useState(false);
   const router = useRouter();
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setDrawer(open);
+  };
+
   return (
-    <Paper>
-      <MenuBar>
-        <MenuItem target="/">Home</MenuItem>
-      </MenuBar>
-    </Paper>
+    <AppBar position="relative">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Jobjörns ordspel
+        </Typography>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+      <Drawer anchor="right" open={drawer} onClose={toggleDrawer(false)}>
+        <List>
+          {user && <ListItem>{user.name}</ListItem>}
+          <ListItemButton key={'Pågående spel'}>
+            <ListItemIcon>
+              <GridOnIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Pågående spel'} />
+          </ListItemButton>
+
+          <Divider />
+
+          {user ? (
+            <ListItemButton
+              key={'Logga ut'}
+              component="a"
+              href="/api/auth/logout"
+            >
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary={'Logga ut'} />
+            </ListItemButton>
+          ) : (
+            <ListItemButton
+              key={'Logga in'}
+              component="a"
+              href="/api/auth/login"
+            >
+              <ListItemIcon>
+                <LoginIcon />
+              </ListItemIcon>
+              <ListItemText primary={'Logga in'} />
+            </ListItemButton>
+          )}
+        </List>
+      </Drawer>
+    </AppBar>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,93 @@
 import React from 'react';
-import { Box, Container, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  Paper,
+  Stack,
+  Typography
+} from '@mui/material';
 import { NextPage } from 'next';
-import Head from 'next/head';
 import { Menu } from 'components/Menu';
 import { useUser } from '@auth0/nextjs-auth0';
 import { Board } from 'components/Board';
 
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'© '}
+      <Link color="inherit" href="https://jobjorn.se/">
+        Jobjörn Folkesson
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
 const IndexPage: NextPage<{}> = () => {
   const { user, error, isLoading } = useUser();
-  return (
-    <Container maxWidth="md">
-      <Head>
-        <title>Jobjörns ordspel</title>
-      </Head>
+
+  return user ? (
+    <>
       <Menu />
-      {user ? (
-        <div>
-          Hej {user.name}! <a href="/api/auth/logout">Logga ut</a>
-        </div>
-      ) : (
-        <div>
-          Du är inte inloggad. <a href="/api/auth/login">Logga in</a>
-        </div>
-      )}
-      <Box>
-        <Paper>
-          <Board />
-        </Paper>
+      <Container maxWidth="md">
+        <Box>
+          <Paper>
+            <Board />
+          </Paper>
+        </Box>
+      </Container>
+    </>
+  ) : (
+    <>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Jobjörns ordspel
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            Här kan du prova Jobjörns ordspel!
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button variant="contained" href="/api/auth/login">
+              Logga in
+            </Button>
+            <Button variant="outlined">Berätta mer</Button>
+          </Stack>
+        </Container>
       </Box>
-    </Container>
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Copyright />
+      </Box>
+    </>
   );
 };
 
