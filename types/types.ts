7976f3b7166ce5168ede77bd4ae0282c904c1a9f
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-05-24 21:14:47 +0200

    typer + fungerande användarlisthämtning

--- types/types.ts
+++ types/types.ts
@@ -1,11 +1,16 @@
-export interface Fruit {
-  id: number;
-  fruit: string;
-  position: number;
-  pivot?: boolean;
-}
-
 export interface Tile {
   letter: string;
   placed: string;
 }
+
+export interface SuccessResponse<T> {
+  success: true;
+  data: T;
+}
+
+export interface ErrorResponse {
+  success: false;
+  error: Error;
+}
+
+export type ResponseType<T> = SuccessResponse<T> | ErrorResponse;
