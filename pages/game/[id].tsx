ef18cfca8d86a9075957a73722ff7e191d62b687
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-10-14 07:13:17 +0200

    en liten rubrik i spelvyn

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -14,6 +14,7 @@ import Container from '@mui/material/Container';
 import CircularProgress from '@mui/material/CircularProgress';
 import router from 'next/router';
 import { Board } from 'components/Board';
+import { Typography } from '@mui/material';
 
 const NewGamePage: NextPage<{}> = () => {
   const [userWithId, setUserWithId] = useState<User>();
@@ -48,6 +49,7 @@ const NewGamePage: NextPage<{}> = () => {
       >
         <Menu />
         <Container maxWidth="sm">
+          <Typography variant="h2">Spel {gameId}</Typography>
           <Board />
         </Container>
         <Footer />
