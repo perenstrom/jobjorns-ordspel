6451db25ce4463f4199a88af00811a505abe9db5
Jobjörn Folkesson <jobjorn@jobjorn.se>
2022-12-06 13:31:57 +0100

    det går nu att spara drag i databasen

--- pages/api/games/[id].tsx
+++ pages/api/games/[id].tsx
@@ -33,8 +33,74 @@ const getGame = async (gameId: number) => {
   }
 };
 
+const submitTurn = async (
+  gameId: number,
+  userId: number,
+  latestPlayedWord: string,
+  latestPlayedBoard: string
+) => {
+  console.log('nu kör vi submitTurn i APIt');
+
+  try {
+    const updateResult = await prisma.usersOnGames.update({
+      data: {
+        latestPlayedWord: latestPlayedWord,
+        latestPlayedBoard: latestPlayedBoard,
+        latestPlayedTime: new Date()
+      },
+      where: {
+        userId_gameId: {
+          userId: userId,
+          gameId: gameId
+        }
+      }
+    });
+    if (updateResult !== null) {
+      return { message: 'Draget sparades' };
+    } else {
+      throw new Error(
+        'Något gick fel i sparandet av draget, updateResult var null'
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error('Det blev ett error som fångades i terminalen');
+  }
+};
+
+interface PostRequestBody {
+  userId: number;
+  latestPlayedWord: string;
+  latestPlayedBoard: string;
+}
+
 const games = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
+    return new Promise((resolve) => {
+      const { userId, latestPlayedWord, latestPlayedBoard }: PostRequestBody =
+        req.body;
+      console.log(req.body);
+
+      submitTurn(
+        parseInt(req.query.id as string, 10),
+        userId,
+        latestPlayedWord,
+        latestPlayedBoard
+      )
+        .then((result) => {
+          console.log('result', result);
+          res.status(200).json(result);
+          resolve('');
+        })
+        .catch((error) => {
+          res.status(500).end(error);
+          resolve('');
+        })
+        .finally(async () => {
+          await prisma.$disconnect();
+        });
+    });
+  } else if (req.method === 'GET') {
     return new Promise((resolve) => {
       getGame(parseInt(req.query.id as string, 10))
         .then((result) => {
