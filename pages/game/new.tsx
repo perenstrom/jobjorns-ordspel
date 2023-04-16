b60e27c77ed054a5e483c557939da6291f916acf
Jobjörn Folkesson <jobjorn@jobjorn.se>
2023-04-16 11:55:14 +0200

    ny lista för spelstart

--- pages/game/new.tsx
+++ pages/game/new.tsx
@@ -7,7 +7,7 @@ import {
   WithPageAuthRequiredProps
 } from '@auth0/nextjs-auth0';
 import { Footer } from 'components/Footer';
-import { getUser, listUsers, startGame } from 'services/local';
+import { listUsers, startGame } from 'services/local';
 import { User } from '@prisma/client';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
@@ -15,12 +15,13 @@ import CircularProgress from '@mui/material/CircularProgress';
 import {
   Avatar,
   Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  Grid,
-  Stack,
-  Typography
+  Checkbox,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Stack
 } from '@mui/material';
 import { gravatar } from 'services/helpers';
 
@@ -28,7 +29,6 @@ const NewGamePage: NextPage<{}> = () => {
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState<User[]>([]);
   const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
-  const [userWithId, setUserWithId] = useState<User>();
 
   const { user } = useUser();
 
@@ -46,47 +46,18 @@ const NewGamePage: NextPage<{}> = () => {
   };
 
   useEffect(() => {
-    const fetchUserWithId = async () => {
-      if (user && user.email) {
-        const newUserWithId = await getUser(user.email);
-
-        if (newUserWithId.success) {
-          setUserWithId(newUserWithId.data);
-        }
-      }
-    };
-
-    fetchUserWithId();
-  }, [user]);
-
-  useEffect(() => {
     const fetchUsers = async () => {
-      const usersList = await listUsers();
-      if (usersList.success && userWithId) {
-        const filteredList = usersList.data.filter(
-          (user) => user.sub !== userWithId.sub
-        );
-        setUsers(filteredList);
+      const newUsers = await listUsers();
+      if (newUsers.success) {
+        setUsers(newUsers.data);
         setLoading(false);
       }
     };
 
     fetchUsers();
-  }, [userWithId]);
+  }, []);
 
-  const styleSelected = (user: User) => {
-    const index = selectedUsers.indexOf(user);
-    if (index === -1) {
-      return {};
-    } else {
-      return {
-        borderColor: 'whitesmoke',
-        filter: 'drop-shadow(0px 0px 3px white)'
-      };
-    }
-  };
-
-  if (userWithId && !loading) {
+  if (user && !loading) {
     return (
       <Box
         sx={{
@@ -99,29 +70,44 @@ const NewGamePage: NextPage<{}> = () => {
         }}
       >
         <Menu />
-        <Container maxWidth="md">
-          <Grid container spacing={2}>
-            {users.map((user, index) => (
-              <Grid item sm={6} md={4} key={index} style={{ width: '100%' }}>
-                <CardActionArea onClick={() => toggleUser(user)}>
-                  <Card variant="outlined" style={styleSelected(user)}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                      <CardContent sx={{ flexGrow: 0 }}>
-                        <Avatar src={gravatar(user.email)} />
-                      </CardContent>
-                      <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography>{user.name}</Typography>
-                      </CardContent>
-                    </Box>
-                  </Card>
-                </CardActionArea>
-              </Grid>
-            ))}
-          </Grid>
+        <Container maxWidth="sm" sx={{ flexGrow: 1 }}>
+          <List>
+            {users.map(
+              (listUser, index) =>
+                user.sub !== listUser.sub && (
+                  <ListItem
+                    key={index}
+                    onClick={() => toggleUser(listUser)}
+                    secondaryAction={
+                      <Checkbox
+                        edge="end"
+                        checked={selectedUsers.indexOf(listUser) !== -1}
+                      />
+                    }
+                    disablePadding
+                  >
+                    <ListItemButton
+                      selected={selectedUsers.indexOf(listUser) !== -1}
+                    >
+                      <ListItemAvatar>
+                        <Avatar src={gravatar(listUser.email)} />
+                      </ListItemAvatar>
+                      <ListItemText primary={listUser.name} />
+                    </ListItemButton>
+                  </ListItem>
+                )
+            )}
+          </List>
+
           <Stack direction="row" spacing={1} sx={{ my: 1 }}>
             <Button
               variant="contained"
-              onClick={() => startGame(userWithId, selectedUsers)}
+              onClick={() =>
+                startGame(
+                  users.find((u) => u.sub === user.sub) as User,
+                  selectedUsers
+                )
+              }
             >
               Starta spelet
             </Button>
