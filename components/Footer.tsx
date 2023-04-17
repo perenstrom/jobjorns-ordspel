6bd92d9f202ea8e950b460286e9e509c9a18787a
Jobjörn Folkesson <jobjorn@jobjorn.se>
2023-04-17 22:46:00 +0200

    buggfix + bort med screenen

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
-import { PrivacyScreen } from './PrivacyScreen';
 
 export const Footer: React.FC<{}> = (props) => {
   return (
     <Box sx={{ bgcolor: 'background.paper', p: 1 }} component="footer">
-      <PrivacyScreen />
       <Typography
         variant="body2"
         color="text.secondary"
