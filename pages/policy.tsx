d4279ded813a8c4ea3c05a2e4ada7d232da8af5f
jobjorn <jobjorn@jobjorn.se>
2023-08-11 13:37:53 +0200

    statiska sidor som inte behöver logincheck

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { NextPage } from 'next';
 import { Menu } from 'components/Menu';
-import { useUser } from '@auth0/nextjs-auth0/client';
 import { Footer } from 'components/Footer';
-import { addUser } from 'services/local';
 import Head from 'next/head';
 import { faviconString } from 'services/helpers';
-import { Loading } from 'components/Loading';
 import { Policy } from 'components/Policy';
 
 const PolicyPage: NextPage<{}> = () => {
-  const { user, isLoading } = useUser(); // härifrån finns också error att ta ut
-
-  if (typeof user !== 'undefined' && isLoading === false) {
-    addUser(user);
-  }
-
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
     <>
       <Box
